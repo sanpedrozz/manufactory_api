e854434c1fea2d227b6aece0310f56a1f98b8fbd
petrl <sup686@gmail.com>
2024-06-19 22:46:00 +0300

    logging off

--- src/database.py
+++ src/database.py
@@ -1,12 +1,16 @@
 # src/database.py
 
+import logging
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from src.config import settings as global_settings
+
+# Настроить логгер SQLAlchemy
+logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
 
 engine = create_async_engine(
     global_settings.asyncpg_url,
     future=True,
-    echo=True,
+    echo=False,
 )
 
 AsyncSessionFactory = async_sessionmaker(
