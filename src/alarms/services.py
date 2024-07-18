0c8fb4f1678bf6fd7a8d5d9b73dbd08dfa5a8882
petrl <sup686@gmail.com>
2024-07-18 16:01:18 +0300

    fix video download

--- src/alarms/services.py
+++ src/alarms/services.py
@@ -27,15 +27,16 @@ async def alarm_message(db: AsyncSession, alarm: Alarm):
                f'{alarm_data.tag}')
 
     # Выгрузка видео
-    cameras_list = await Place.get_cameras_by_place_id(db, alarm.place_id)
     path_list = []
-    for camera in cameras_list:
-        path = await get_video(camera.camera_info, current_time)
-        if path is not None:
-            path_list.append(path)
-        else:
-            message = (f'{message}\n'
-                       f'#ВИДЕО_НЕ_ПОЛУЧИЛОСЬ_ВЫГРУЗИТЬ_КАМЕРА_{camera.comment}')
+    if alarm_data.camera:
+        cameras_list = await Place.get_cameras_by_place_id(db, alarm.place_id)
+        for camera in cameras_list:
+            path = await get_video(camera.camera_info, current_time)
+            if path is not None:
+                path_list.append(path)
+            else:
+                message = (f'{message}\n'
+                           f'#ВИДЕО_НЕ_ПОЛУЧИЛОСЬ_ВЫГРУЗИТЬ_КАМЕРА_{camera.comment}')
 
     if path_list:
         await send_video(path_list, message, place.message_thread_id)
