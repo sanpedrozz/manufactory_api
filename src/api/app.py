fc59a812bf299cb404c1fab3666b2cb83ec1a139
petrl <sup686@gmail.com>
2024-10-04 21:58:20 +0300

    dell fastapi web page

--- src/api/app.py
+++ src/api/app.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from src.api.alarm.router import router as alarms
 from src.api.operation_history.router import router as operation_history
-from fastapi.staticfiles import StaticFiles
-from pathlib import Path
 
 app = FastAPI(title="Manufactory API", version="1.0.0")
 
@@ -17,6 +15,3 @@ app.include_router(
     prefix="/operation_history",
     tags=["operation_history"]
 )
-
-app.mount("/static", StaticFiles(directory=Path(__file__).parent / "operation_history/static", html=True),
-          name="static")
