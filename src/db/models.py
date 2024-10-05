2adc78b1f0a3da3ed63e650534110c0d1dd550dc
petrl <sup686@gmail.com>
2024-10-05 08:58:59 +0300

    fix column name

--- src/db/models.py
+++ src/db/models.py
@@ -44,7 +44,7 @@ class Place(Base):
     id = Column(BigInteger, primary_key=True)
     name = Column(Text)
     message_thread_id = Column(String, default="General", nullable=False)
-    controller_ip = Column(String(45), nullable=True)
+    ip = Column(String(45), nullable=True)
 
     operations = relationship("OperationHistory", back_populates="place", post_update=True)
     alarm_histories = relationship("AlarmHistory", back_populates="place", post_update=True)
