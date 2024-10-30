8a06e33400231923667d0a3f958304d48904acf1
petrl <sup686@gmail.com>
2024-10-30 17:03:29 +0300

    add get_data_test

--- src/api/printer/router.py
+++ src/api/printer/router.py
@@ -2,9 +2,16 @@ import random
 
 from fastapi import APIRouter, HTTPException
 
-from src.api.printer.services import print_label_service
 from src.api.printer.schemas import LabelRequest
+from src.api.printer.services import print_label_service
+
 router = APIRouter()
+
+# Инициализируем начальные значения
+x_value = 100
+y_value = 100
+x_max = 2700
+y_max = 1900
 
 
 @router.get("/get_data", name="get_print_params")
@@ -15,6 +22,32 @@ async def get_printer_params():
         "y": random.randint(100, 1900),
         "a": random.choice([False, True]),
     }
+
+    # 10% шанс вернуть {"result": "done"}
+    if random.random() < 0.1:
+        return {"result": "done"}
+
+    return data
+
+
+@router.get("/get_data_test", name="get_data_test")
+async def get_data_test():
+    global x_value, y_value
+
+    data = {
+        "id": random.randint(100000000000000, 999999999999999),
+        "x": x_value,
+        "y": y_value,
+        "a": random.choice([False, True]),
+    }
+
+    # Обновляем координаты для следующего вызова
+    x_value += 100
+    if x_value > x_max:
+        x_value = 100
+        y_value += 100
+        if y_value > y_max:
+            y_value = 100  # Сбрасываем y после достижения максимума
 
     # 10% шанс вернуть {"result": "done"}
     if random.random() < 0.1:
