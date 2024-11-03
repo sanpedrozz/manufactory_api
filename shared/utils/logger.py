e2c27007d8642dc9003ee445dff86bbe84385202
petrl <sup686@gmail.com>
2024-11-03 22:04:10 +0300

    update logger conf

--- shared/utils/logger.py
+++ shared/utils/logger.py
@@ -1,7 +1,37 @@
 import logging
 
-logger = logging.getLogger("plc_data_hub")
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
-logger.addHandler(handler)
+import colorlog
+
+# Создание объекта colorlog.ColoredFormatter
+formatter = colorlog.ColoredFormatter(
+    '%(asctime)s | %(log_color)s%(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    log_colors={
+        'DEBUG': 'white',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'bold_red',
+    },
+    reset=True,
+    style='%'
+)
+
+# Создание объекта logging.StreamHandler
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+# Создание объекта logger
+logger = logging.getLogger(__name__)
+
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
+
+if __name__ == '__main__':
+    logger.debug('Debug message')
+    logger.info('Info message')
+    logger.warning('Warning message')
+    logger.error('Error message')
+    logger.critical('Critical message')
+    logger.exception('Exception message')
+    raise ValueError("An example exception")
