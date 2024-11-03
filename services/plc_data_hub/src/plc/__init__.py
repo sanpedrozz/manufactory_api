98717d1d8357082a46269d000c885d4dc99f11be
petrl <sup686@gmail.com>
2024-11-03 22:06:22 +0300

    add plc

---
+++ services/plc_data_hub/src/plc/__init__.py
@@ -0,0 +1,25 @@
+from .plc_client import PLCClient
+from .plc_data_models import (
+    DataType,
+    StringDataType,
+    IntDataType,
+    RealDataType,
+    BoolDataType,
+    UIntDataType,
+    USIntDataType,
+    DIntDataType,
+    models
+)
+
+__all__ = [
+    "PLCClient",
+    "DataType",
+    "StringDataType",
+    "IntDataType",
+    "RealDataType",
+    "BoolDataType",
+    "UIntDataType",
+    "USIntDataType",
+    "DIntDataType",
+    "models"
+]
---
+++ services/plc_data_hub/src/plc/plc_client.py
@@ -0,0 +1,110 @@
+import logging
+from functools import wraps
+from time import sleep
+
+from snap7 import client
+
+from shared.utils.logger import logger
+
+logging.getLogger("snap7").setLevel(logging.CRITICAL)
+
+
+def reconnect_on_fail(delay=5):
+    """
+    Декоратор, обеспечивающий повторное подключение к контроллеру PLC при сбоях соединения.
+    :param delay: Задержка (в секундах) между попытками переподключения
+    :return: Декорированная функция
+    """
+
+    def decorator(function):
+        @wraps(function)
+        def wrapper(self, *args, **kwargs):
+            while True:
+                try:
+                    if self.connected:
+                        return function(self, *args, **kwargs)
+                    self.connect()
+                except Exception as error:
+                    logger.warning(f'PLC error for {self.ip}: {error}')
+                    self.disconnect()
+                    sleep(delay)
+
+        return wrapper
+
+    return decorator
+
+
+class PLCClient:
+    def __init__(self, ip: str):
+        self.ip = ip
+        self.client = client.Client()
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.disconnect()
+
+    @property
+    def connected(self):
+        """
+        Проверяет, установлено ли соединение с контроллером PLC
+        :return: True, если соединение установлено, иначе False
+        """
+        return self.client.get_connected()
+
+    def connect(self):
+        """
+        Устанавливает соединение с контроллером PLC.
+        """
+        if not self.connected:
+            self.client.connect(self.ip, 0, 1)
+            logger.info(f'Connected to PLC at {self.ip}')
+
+    def disconnect(self):
+        """
+        Разрывает соединение с контроллером PLC.
+        """
+        if self.connected:
+            self.client.disconnect()
+            logger.info(f'Disconnected from PLC at {self.ip}')
+
+    @reconnect_on_fail()
+    def read_data(self, db_number: int, offset: int, size: int):
+        """
+        Читает данные из указанной области данных (DB) контроллера PLC.
+        :param db_number: Int - Номер области данных (DB).
+        :param offset: Int - Смещение начального байта в области данных.
+        :param size: Int - Размер читаемых данных (в байтах).
+        :return: Прочитанные данные.
+        """
+        return self.client.db_read(db_number, offset, size)
+
+    @reconnect_on_fail()
+    def write_data(self, db_number: int, start: int, data: bytearray):
+        """
+        Записывает данные в указанную область данных (DB) контроллера PLC
+        :param db_number: Int - Номер области данных (DB).
+        :param start: Int - Смещение начального байта в области данных.
+        :param data: Int - Размер читаемых данных (в байтах).
+        :return: Результат операции записи (количество записанных байт).
+        """
+        return self.client.db_write(db_number, start, data)
+
+
+# Предположим, что ваш класс PLCClient уже определен с методами __enter__ и __exit__
+
+plc_ip = '192.168.1.15'
+
+# Используем конструкцию `with` для автоматического подключения и отключения от PLC
+c = 0
+with PLCClient(plc_ip) as plc_client:
+    while True:
+        # Внутри блока `with` соединение с PLC установлено, и мы можем работать с plc_client
+        data = plc_client.read_data(db_number=1, offset=0, size=10)
+        c += 1
+        print(c)
+        sleep(0.1)
+
+# При выходе из блока `with` метод __exit__ автоматически вызовет disconnect
