7e4929514894e93ad30c1bde62bcf8dd6c2bdedd
petrl <sup686@gmail.com>
2024-11-05 13:42:31 +0300

    make full path for start

--- services/plc_data_hub/src/app.py
+++ services/plc_data_hub/src/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from src.api.router import router
+from services.plc_data_hub.src.api.router import router
 
 app = FastAPI(root_path="/plc-data", title="PLC API", version="1.0.0")
 
