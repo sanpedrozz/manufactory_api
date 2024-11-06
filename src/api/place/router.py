be1784d0a91b5732db5463a42766a549afe37f55
petrl <sup686@gmail.com>
2024-11-06 16:29:44 +0300

    dell places.data_for_read

--- src/api/place/router.py
+++ src/api/place/router.py
@@ -1,7 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.api.place.schemas import DataBlock
 from src.database import get_db
 from src.db.models import Place
 
@@ -11,16 +10,3 @@ router = APIRouter()
 @router.get("/places")
 async def get_places(db: AsyncSession = Depends(get_db)):
     return await Place.get_all(db)
-
-
-@router.post("/places/{place_id}/update_data_for_read")
-async def update_data_for_read(place_id: int, data_for_read: list[DataBlock], db: AsyncSession = Depends(get_db)):
-    place = await Place.get_place_by_id(db, place_id)
-    data_for_read_json = [block.dict() for block in data_for_read]
-
-    if not place:
-        raise HTTPException(status_code=404, detail=f"Place with id {place_id} not found")
-
-
-    await place.update_data_for_read(db, data_for_read_json)
-    return {"message": "Data updated successfully"}
--- src/db/models.py
+++ src/db/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from typing import List
 
 from fastapi import HTTPException, status
-from sqlalchemy import Column, ForeignKey, Text, Integer, BigInteger, DateTime, String, JSON
+from sqlalchemy import Column, ForeignKey, Text, Integer, BigInteger, DateTime, String
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
@@ -45,7 +45,6 @@ class Place(Base):
     name = Column(Text)
     message_thread_id = Column(String, default="General", nullable=False)
     ip = Column(String(45), nullable=True)
-    data_for_read = Column(JSON, nullable=True)
 
     operations = relationship("OperationHistory", back_populates="place", post_update=True)
     alarm_histories = relationship("AlarmHistory", back_populates="place", post_update=True)
@@ -91,22 +90,6 @@ class Place(Base):
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail=str(ex)
-            ) from ex
-
-    async def update_data_for_read(self, db: AsyncSession, new_data):
-        """
-        Update column data_for_read for current Place.
-        :param db: The database session.
-        :param new_data: Новые данные для записи в колонку data_for_read.
-        """
-        try:
-            # Вызов метода update из базового класса для обновления конкретного поля
-            await self.update(db, data_for_read=new_data)
-        except SQLAlchemyError as ex:
-            await db.rollback()
-            raise HTTPException(
-                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                detail=f"Ошибка при обновлении data_for_read: {str(ex)}"
             ) from ex
 
 
