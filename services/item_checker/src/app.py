6892b175616c0ae24c516953ea3868d735d1d236
petrl <sup686@gmail.com>
2024-11-07 16:17:06 +0300

    fix

--- services/item_checker/src/app.py
+++ services/item_checker/src/app.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from services.item_checker.src.routers import view
+from services.item_checker.src.routers import routers
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 # Подключаем маршруты
-app.include_router(view.router)
+app.include_router(routers.router)
 
 # Подключение статических файлов (для стилей)
 templates = Jinja2Templates(directory="templates")
