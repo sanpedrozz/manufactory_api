e73d427a3f3b353d1e2747082ee75696c806b007
petrl <sup686@gmail.com>
2024-11-19 23:25:35 +0300

    fix

--- shared/db/industrial_stg/database.py
+++ shared/db/industrial_stg/database.py
@@ -8,7 +8,7 @@ from shared.config.config import settings as global_settings
 logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
 
 engine = create_async_engine(
-    global_settings.industrial_db_url,
+    str(global_settings.industrial_db_url),
     future=True,
     echo=False,
 )
