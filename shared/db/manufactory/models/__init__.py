b93535fb7e4f2409cfa0522b257d319fc938cf20
petrl <sup686@gmail.com>
2024-11-19 22:08:44 +0300

    fix

---
+++ shared/db/manufactory/models/__init__.py
@@ -0,0 +1,14 @@
+from .models import (OperationHistory,
+                     Place,
+                     AlarmMessages,
+                     AlarmHistory,
+                     PLCData)
+
+
+__all__ = [
+    "OperationHistory",
+    "Place",
+    "AlarmMessages",
+    "AlarmHistory",
+    "PLCData"
+]
