ab7fc62a03bb285a122f6440a40080beea608c0e
petrl <sup686@gmail.com>
2024-12-02 09:10:18 +0300

    update name

--- services/plc_data_hub/src/app.py
+++ services/plc_data_hub/src/app.py
@@ -1,8 +1,39 @@
-from fastapi import FastAPI
+import asyncio
+import logging
 
-from services.plc_data_hub.src.api.router import router
+from services.plc_data_hub.src.core.get_plcs import fetch_places
+from services.plc_data_hub.src.core.plc_reader import Reader
+from shared.db.manufactory.database import AsyncSessionFactory
+from shared.utils import logger
 
-app = FastAPI(root_path="/plc-data", title="PLC API", version="1.0.0")
+logging.getLogger("main").setLevel(logging.WARNING)
 
-# Подключение API
-app.include_router(router.router)
+
+async def initialize_readers():
+    async with AsyncSessionFactory() as session:  # Создаём реальную сессию базы данных
+        places = await fetch_places()  # Получаем список PLC
+
+        if not places:
+            logger.info("Нет подходящих PLC для подключения.")
+            return
+
+        tasks = []
+        for place in places:
+            logger.info(f"Инициализация Reader для {place.name} ({place.ip})")
+
+            # Передаём реальную сессию
+            async with AsyncSessionFactory() as place_session:
+                reader = Reader(ip=place.ip, db_session=place_session)
+                tasks.append(reader.run())
+
+        results = await asyncio.gather(*tasks, return_exceptions=True)
+        for result in results:
+            if isinstance(result, Exception):
+                logger.error(f"Ошибка в задаче: {result}")
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(initialize_readers())
+    except Exception as e:
+        logger.error(f"Ошибка при запуске приложения: {e}")
